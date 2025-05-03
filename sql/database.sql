a4b997105739c010d8a3e39698afce754d2e3579
Fidji32 <matthis.vandevyver@gmail.com>
2025-05-03 13:07:05 +0200

    feat: add file create database

---
+++ sql/database.sql
@@ -0,0 +1,12 @@
+-- Database: soradrive
+
+CREATE DATABASE soradrive
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'fr-FR'
+    LC_CTYPE = 'fr-FR'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
