9d4b934c664c8e9cb1baa90ef6bdab75baf413b2
Durred <edorrock33@gmail.com>
2025-05-16 11:36:41 +0200

    Correctif connexion

--- views/pages/PHP/homeNotConnected.php
+++ views/pages/PHP/homeNotConnected.php
@@ -12,6 +12,7 @@ if (session_status() === PHP_SESSION_NONE) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>SoraDrive - Accueil</title>
   <link rel="stylesheet" href="../CSS/IndexStyle.css" />
+  <script src="../../../public/assets/js/login.js" defer></script>
 </head>
 <body>
   <header class="main-header">
